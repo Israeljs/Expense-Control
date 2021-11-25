a5aa21931a5c940b36f1e72ecca5b4c09bf1a784
israel jeronimo <silvaij16@gmail.com>
2021-11-25 19:20:03 -0300

    apagando comentários no arquivo wallet.js

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -25,30 +25,3 @@ const wallet = (state = INITIAL_STATE, action) => {
 };
 
 export default wallet;
-// import { ADD_EXPENSE, REQUEST_CURRENCIES } from '../actions';
-
-// const INITIAL_STATE = {
-//   currencyKeys: {},
-//   currencies: [],
-//   expenses: [],
-// };
-
-// const wallet = (state = INITIAL_STATE, action) => {
-//   switch (action.type) {
-//   case ADD_EXPENSE:
-//     return {
-//       ...state,
-//       expenses: [...state.expenses, { ...action.payLoad, id: state.expenses.length }],
-//     };
-//   case REQUEST_CURRENCIES:
-//     return {
-//       ...state,
-//       currencyKeys: action.currencies,
-//       currencies: action.currencyKeys,
-//     };
-//   default:
-//     return state;
-//   }
-// };
-
-// export default wallet;
